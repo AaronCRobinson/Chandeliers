7c6d0ccdceed2f8be7bd17a0677baed6f6a50372
Aaron C Robinson <robinson.aaron.c@gmail.com>
2021-09-24 13:06:33 +0800

    Chandeliers
    Forked from AJO-Misc

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# Chandeliers
- Cupro's Chandeliers
+# Chandeliers [![RimWorld 1.3](https://img.shields.io/badge/RimWorld-1.3-green.svg?longCache=true&style=plastic)](http://rimworldgame.com/)
+ A very basic 1.3 version of Cupro's Chandeliers from Additional Joy Objects - Misc
---
+++ Source/Chandeliers/CompChandelierBowl.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using Verse;
+using RimWorld;
+
+namespace AJOMisc
+{
+    [StaticConstructorOnStartup]
+    class CompChandelierBowl : ThingComp
+    {
+        private static readonly Material BowlMat = MaterialPool.MatFrom("Cupro/Object/Furniture/Chandelier/New/LargeChandelierGlass", ShaderDatabase.Transparent);
+        
+        public override void PostDraw()
+        {
+            Matrix4x4 matrix = default(Matrix4x4);
+            Vector2 drawSize = base.parent.def.graphicData.drawSize;
+            matrix.SetTRS(base.parent.TrueCenter(),
+                          Quaternion.identity,
+                          new Vector3(drawSize.x, 1f, drawSize.y));
+            Graphics.DrawMesh(MeshPool.plane10, matrix, BowlMat, 0);
+        }
+
+        public override string CompInspectStringExtra()
+        {
+            string ret = base.CompInspectStringExtra();
+            if (Prefs.DevMode) ret += "CompChandelierBowl";
+            return ret;
+        }
+    }
+}
